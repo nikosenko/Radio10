5a5c10e410add29348f12f40480b99ba67bba456
Natalia test <n.i.kosenko@bk.ru>
2024-03-21 13:38:26 +0300

    Reformat code

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -3,35 +3,39 @@ package ru.netology;
 public class Radio {
 
     private int currentStation;
+
     public int getCurrentStation() {
         return currentStation;
     }
 
 
-    public void setCurrentStation (int newCurrentStation){
+    public void setCurrentStation(int newCurrentStation) {
         if (newCurrentStation > 9) {
             newCurrentStation = 0;
         }
-        if (newCurrentStation <0) {
+        if (newCurrentStation < 0) {
             newCurrentStation = 9;
         }
         currentStation = newCurrentStation;
     }
-public void next() {
+
+    public void next() {
         int target = currentStation + 1;
         setCurrentStation(target);
-}
+    }
 
-public void prev() {
-        int target = currentStation-1;
-    setCurrentStation(target);
-}
+    public void prev() {
+        int target = currentStation - 1;
+        setCurrentStation(target);
+    }
 
-private int currentVolume;
+    private int currentVolume;
+
     public int getCurrentVolume() {
         return currentVolume;
     }
-    public void setCurrentVolume (int newCurrentVolume) {
+
+    public void setCurrentVolume(int newCurrentVolume) {
         if (newCurrentVolume > 100) {
             return;
         }
@@ -41,11 +45,12 @@ private int currentVolume;
         currentVolume = newCurrentVolume;
     }
 
-public void increaseVolume() {
+    public void increaseVolume() {
         int target = currentVolume + 1;
         setCurrentVolume(target);
 
-}
+    }
+
     public void reduceVolume() {
         int target = currentVolume - 1;
         setCurrentVolume(target);
